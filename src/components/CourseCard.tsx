e54aff2bdafe6b0301da9821305ac4781619a6e2
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-14 00:46:27 +0300

    Enhancement: Update CourseCard component to improve navigation by wrapping course image and name links with correct routing paths.

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -4,10 +4,12 @@ import type { Course } from "../types";
 const CourseCard = ({ course }: { course: Course }) => {
   return (
     <div className="rounded-md bg-[#1b1b1b] overflow-hidden">
-      <img src={course.image} alt={course.name} className="w-full" />
+      <Link to={`/courses/${course.id}`}>
+        <img src={course.image} alt={course.name} className="w-full" />
+      </Link>
       <div className="py-8 px-5">
         <h2 className="font-bold text-2xl text-[#f9f9f9]">
-          <Link to={`course/${course.id}`}>{course.name}</Link>
+          <Link to={`/courses/${course.id}`}>{course.name}</Link>
         </h2>
         <span className="text-sm font-medium text-[#939393]">
           {course.instructor}
