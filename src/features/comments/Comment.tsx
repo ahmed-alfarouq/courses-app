85c2ae2744d3132570d3ab17fa42a85f1cfd7541
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-17 02:47:52 +0300

    Enhancement: Adjust avatar image size in Comment component for better responsiveness on mobile screens.

--- src/features/comments/Comment.tsx
+++ src/features/comments/Comment.tsx
@@ -6,7 +6,7 @@ const Comment = ({ avatar, name, date, comment }: CommentProps) => {
 
   return (
     <div className="border-b-1 last:border-0 border-primary-border pb-5 px-1 flex gap-4">
-      <img src={avatar} alt={name} className="size-18 rounded-full" />
+      <img src={avatar} alt={name} className="size-13 md:size-18 rounded-full" />
       <div>
         <h3 className="text-[15px] font-semibold text-[#6C6C6C]">{name}</h3>
         <span className="text-[15px] text-gray">{commentDate}</span>
