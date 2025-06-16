d6176629613ebc7db7a3f7ee12e7e8b8c1946881
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-16 21:15:55 +0300

    Feature: Add StudentProgress component and integrate it into CourseSections.

---
+++ src/features/StudentProgress.tsx
@@ -0,0 +1,37 @@
+const StudentProgress = ({ percent }: { percent: number }) => {
+  return (
+    <div className="relative my-14">
+      <div
+        className="w-full h-[5px] bg-[#E6E6E6] rounded-full"
+        role="progressbar"
+      >
+        <div
+          className="bg-[#6ABD8A] h-[5px] rounded-full"
+          style={{ width: `${percent}%` }}
+        ></div>
+      </div>
+
+      <span
+        className="absolute -top-13 text-[11px] text-[#485293] border-2 border-primary-border rounded-full p-2 after:border-3 after:border-primary-border after:border-r-transparent after:border-l-transparent after:border-b-transparent after:absolute after:-bottom-3 after:left-1/2 after:-translate-x-1/2"
+        style={{
+          left: `${percent}%`,
+          transform: `translateX(-50%)`,
+        }}
+      >
+        You
+      </span>
+
+      <span
+        className="absolute -bottom-6 text-[11px] text-[#485293]"
+        style={{
+          left: `${percent}%`,
+          transform: `translateX(-50%)`,
+        }}
+      >
+        {percent}%
+      </span>
+    </div>
+  );
+};
+
+export default StudentProgress;
--- src/features/courseSection/CourseSections.tsx
+++ src/features/courseSection/CourseSections.tsx
@@ -3,6 +3,7 @@ import { cn } from "@sglara/cn";
 import CourseSection from "./CourseSection";
 
 import type { Course } from "../../types";
+import StudentProgress from "../StudentProgress";
 
 const CourseSections = ({
   course,
@@ -16,6 +17,7 @@ const CourseSections = ({
       <h2 className="font-semibold text-2xl md:text-[27px] mb-4">
         Topics for This Course
       </h2>
+      <StudentProgress percent={10} />
       <section className="space-y-8">
         {course.sections.map((section) => (
           <CourseSection key={section.id} section={section} />
