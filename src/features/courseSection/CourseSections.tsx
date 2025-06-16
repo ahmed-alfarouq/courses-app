7500d7874b1bf4ff8b08dbfd6e32a391d385fa34
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-17 01:33:44 +0300

    Enhancement: Calculate and display student progress percentage in CourseSections.

--- src/features/courseSection/CourseSections.tsx
+++ src/features/courseSection/CourseSections.tsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react";
 import { cn } from "@sglara/cn";
 
 import CourseSection from "./CourseSection";
-
-import type { Course } from "../../types";
 import StudentProgress from "../StudentProgress";
+
+import type { Course, StudentProgressProps } from "../../types";
 
 const CourseSections = ({
   course,
@@ -12,12 +13,26 @@ const CourseSections = ({
   course: Course;
   className?: string;
 }) => {
+  const calcStudentProgress = useMemo(() => {
+    const stored = localStorage.getItem(`progress-${course.id}`);
+    if (!stored) return 0;
+
+    const studentProgress: StudentProgressProps = JSON.parse(stored);
+    const lessonsLength = course.sections.reduce(
+      (acc, section) => acc + section.lessons.length,
+      0
+    );
+    const completedLessonsCount = studentProgress.completedLessons.length;
+
+    return Math.round((completedLessonsCount / lessonsLength) * 100);
+  }, [course.id, course.sections]);
+
   return (
     <section className={cn(className)}>
       <h2 className="font-semibold text-2xl md:text-[27px] mb-4">
         Topics for This Course
       </h2>
-      <StudentProgress percent={10} />
+      <StudentProgress percent={calcStudentProgress} />
       <section className="space-y-8">
         {course.sections.map((section) => (
           <CourseSection
