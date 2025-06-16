3774aaa8a5b12f11fd5bf7900c8793855cb08a27
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-16 05:46:40 +0300

    Enhancement: Add 3xl breakpoint to CSS and integrate VideoPlayer component into CourseDetails.

--- src/pages/CourseDetails.tsx
+++ src/pages/CourseDetails.tsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 
-import ErrorOverlay from "../components/ErrorOverlay";
-import { useCoursesContext } from "../context/coursesContext";
 import Breadcrumb from "../components/Breadcrumb";
+import VideoPlayer from "../components/VideoPlayer";
+import ErrorOverlay from "../components/ErrorOverlay";
+
+import { useCoursesContext } from "../context/coursesContext";
 
 const CourseDetails = () => {
   const { id } = useParams<{ id: string }>();
@@ -24,10 +26,22 @@ const CourseDetails = () => {
 
   return (
     <section>
-      <header className="py-3 px-3 md:px-12 bg-[#F5F9FA] space-y-1 md:space-y-4">
-        <Breadcrumb items={breadcrumbItems} />
-        <h1 className="text-3xl md:text-4xl font-medium leading-9">{course.name}</h1>
+      <header className="py-3 px-3 md:px-12 3xl:px-0 bg-[#F5F9FA] space-y-1 md:space-y-4">
+        <div className="3xl:container 3xl:mx-auto">
+          <Breadcrumb items={breadcrumbItems} />
+          <h1 className="text-3xl md:text-4xl font-medium leading-9">
+            {course.name}
+          </h1>
+        </div>
       </header>
+      <main className="py-3 px-3 md:px-12 3xl:px-0">
+        <div className="3xl:container 3xl:mx-auto flex gap-2 flex-col md:flex-row">
+          <section className="w-full md:w-3/5">
+            <VideoPlayer url={course.sections[0].lessons[0].url} />
+          </section>
+          <section>sidebar</section>
+        </div>
+      </main>
     </section>
   );
 };
