0a8bb56a4032b2d96e55afffc0aa1e19f27b902f
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-16 09:17:48 +0300

    Enhancement: Add primary border color variable and update CourseMaterialItem to use it.

--- src/features/courseMaterials/CourseMaterialItem.tsx
+++ src/features/courseMaterials/CourseMaterialItem.tsx
@@ -6,7 +6,7 @@ export const CourseMaterialItem = ({
   description,
 }: CourseMaterialItemProps) => {
   return (
-    <div className="flex justify-between items-center gap-1 border-b-1 border-gray/50 md:last:border-0 py-3">
+    <div className="flex justify-between items-center gap-1 border-b-1 border-primary-border md:last:border-0 py-3">
       <div className="flex items-center gap-2 font-light">
         {icon}
         <p className="text-lg lg:text-xl whitespace-nowrap">{title}:</p>
