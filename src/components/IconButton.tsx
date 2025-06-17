dc1ac1148a77dcc3486a79fdc652264f33b69d32
ahmed-al-farouq <mediocre23534@gmail.com>
2025-06-18 00:27:39 +0300

    Refactor: Optimize IconButton component using React.memo for improved performance.

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,29 +2,26 @@ import { cn } from "@sglara/cn";
 
 import type { IconButtonProps } from "../types";
 import ToolTip from "./ToolTip";
+import React from "react";
 
-const IconButton = ({
-  toolTipId,
-  icon,
-  ariaLabel,
-  className,
-  onClick,
-}: IconButtonProps) => {
-  return (
-    <button
-      type="button"
-      className={cn(
-        "relative border border-gray/50 rounded-full p-3 text-gray cursor-pointer group",
-        className
-      )}
-      aria-describedby={toolTipId || ""}
-      aria-label={ariaLabel}
-      onClick={onClick}
-    >
-      {toolTipId && <ToolTip id={toolTipId} text={ariaLabel} />}
-      {icon}
-    </button>
-  );
-};
+const IconButton = React.memo(
+  ({ toolTipId, icon, ariaLabel, className, onClick }: IconButtonProps) => {
+    return (
+      <button
+        type="button"
+        className={cn(
+          "relative border border-gray/50 rounded-full p-3 text-gray cursor-pointer group",
+          className
+        )}
+        aria-describedby={toolTipId || ""}
+        aria-label={ariaLabel}
+        onClick={onClick}
+      >
+        {toolTipId && <ToolTip id={toolTipId} text={ariaLabel} />}
+        {icon}
+      </button>
+    );
+  }
+);
 
 export default IconButton;
